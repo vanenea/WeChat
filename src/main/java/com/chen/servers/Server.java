ab885d3c698081c7b72045b7c4aae5e858cbfe85
vanenea <576700973@qq.com>
2018-09-17 21:46:36 +0800

    17.September.2018

--- src/main/java/com/chen/servers/Server.java
+++ src/main/java/com/chen/servers/Server.java
@@ -1,16 +1,18 @@
 package com.chen.servers;
 
 import java.io.IOException;
+import java.io.InputStream;
+import java.io.OutputStream;
 import java.net.ServerSocket;
 import java.net.Socket;
 import java.util.concurrent.ExecutorService;
 import java.util.concurrent.Executors;
-import java.util.concurrent.ThreadPoolExecutor;
 
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
 import com.chen.utils.Config;
+import com.chen.utils.RequestCommand;
 
 /**
  * 服务端
@@ -29,6 +31,7 @@ public class Server {
 			ExecutorService pool = Executors.newFixedThreadPool(10);
 			while(true) {
 				Socket socket = serverSocket.accept();
+				pool.execute(new HandServer(socket));
 				LOGGER.info("客户端连接进来");
 				count ++;
 				LOGGER.info("当前在线人数:"+count);
@@ -46,7 +49,119 @@ public class Server {
 		}
 	}
 	
-	public void handServer() {
+	/**
+	 * 	处理服务器类
+	 * @author chen
+	 *
+	 */
+	class HandServer implements Runnable {
+		//套接字
+		private Socket socket;
+		//输入流
+		private InputStream in;
+		//输出流
+		private OutputStream out;
+		
+		public HandServer(Socket socket) {
+			this.socket = socket;
+		}
+		
+		public void run() {
+			try {
+				in = socket.getInputStream();
+				out = socket.getOutputStream();
+				int command = -1;
+				while((command = in.read())!=-1) {
+					switch (command) {
+					//登陆操作
+					case RequestCommand.LOGIN:
+						LOGGER.info("登陆操作");
+						doLogin();
+						break;
+					//注册操作
+					case RequestCommand.REG:
+						LOGGER.info("注册操作");
+						doReg();
+						break;
+					//广播文件
+					case RequestCommand.SEND_FILE:
+						LOGGER.info("广播文件");
+						doSendFile();
+						break;
+					//请求个人传送文件
+					case RequestCommand.SEND_FILE_TO_ONE:
+						LOGGER.info("请求个人传送文件");
+						doSendFileToOne();
+						break;
+					//请求回复发件人结果
+					case RequestCommand.SEND_FILE_TO_ONE_1:
+						LOGGER.info("请求回复发件人结果");
+						doSendFileToOne1();
+						break;
+					//请求开始传送文件
+					case RequestCommand.SEND_FILE_TO_ONE_2:
+						LOGGER.info("请求开始传送文件");
+						doSendFileToOne2();
+						break;
+					//广播文本信息
+					case RequestCommand.SEND_TEXT:
+						LOGGER.info("广播文本信息");
+						doSendText();
+						break;
+					//文本私聊
+					case RequestCommand.SEND_TEXT_TO_ONE:
+						LOGGER.info("文本私聊");
+						doSendTextToOne();
+						break;
+					default:
+						break;
+					}
+					
+				}
+			} catch (Exception e) {
+				LOGGER.error("", e);
+			}
+		}
+
+		private void doSendTextToOne() {
+			// TODO Auto-generated method stub
+			
+		}
+
+		private void doSendText() {
+			// TODO Auto-generated method stub
+			
+		}
+
+		private void doSendFileToOne2() {
+			// TODO Auto-generated method stub
+			
+		}
+
+		private void doSendFileToOne1() {
+			// TODO Auto-generated method stub
+			
+		}
+
+		private void doSendFileToOne() {
+			// TODO Auto-generated method stub
+			
+		}
+
+		private void doSendFile() {
+			// TODO Auto-generated method stub
+			
+		}
+
+		private void doReg() {
+			// TODO Auto-generated method stub
+			
+		}
+
+		private void doLogin() {
+			// TODO Auto-generated method stub
+			
+		}
 		
 	}
 	
